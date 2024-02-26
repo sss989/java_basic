2478790cdebfc7088a619202f408df3e1603e558
sss989 <sss_9898@naver.com>
2024-02-26 12:04:26 +0900

    0226 commit

--- src/Ch02_variable_type/VariableEx.java
+++ src/Ch02_variable_type/VariableEx.java
@@ -5,7 +5,7 @@ class VariableEx {
         // 변수 선언과 함께 초기화
         int hour =3;
         int minute =5;
-        System.out.println(hour + "시간" + minute +"분"); // hour, minuate 값 출력
+        System.out.println(hour + "시간 " + minute +"분"); // hour, minuate 값 출력
 
         int totalMinute = (hour*60) +minute;
         System.out.println("총 " + totalMinute + "분");
