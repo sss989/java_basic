0bc309b133ce9baa4e93e880a2b5821ed7939d50
sss989 <sss_9898@naver.com>
2024-03-04 23:06:14 +0900

    0304 commit

--- src/Ch02_variable_type/TypeEx.java
+++ src/Ch02_variable_type/TypeEx.java
@@ -8,6 +8,10 @@ public class TypeEx {
         char var4 = 'A'; //문자타입
         float var5 = 3.14f; // f,F를 붙여 float타입임을 증명
         double var6 = 3.14;
+        //논리 타입 -> 연산식 결과를 boolean 변수에 저장해 출력
+        boolean result1 = (var1 == 111);
+        boolean result2 = (var2 != 123);
+        String str = "나는 \"홍길동\" 입니다";
 
         System.out.println("var1은 " + var1);
         System.out.println("var2은 " + var2);
@@ -15,6 +19,9 @@ public class TypeEx {
         System.out.println("var4은 " + var4);
         System.out.println("var5은 " + var5);
         System.out.println("var6은 " + var6);
+        System.out.println("result1은 " + result1);
+        System.out.println("result2은 " + result2);
+        System.out.println("문자열 str은 " + str);
 
     }
 }
